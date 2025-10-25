ac37c57ecf1c698ed532b12e4839949cb9257f78
luan3642 <luan3642@hotmail.com>
2025-10-25 10:42:46 -0300

    getters loombok

---
+++ todolist/src/main/java/br/com/alldirect/luan/todolist/user/UserModel.java
@@ -0,0 +1,15 @@
+package br.com.alldirect.luan.todolist.user;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UserModel {
+    private String userName;
+    private String name;
+    private  String password;
+
+
+
+}
